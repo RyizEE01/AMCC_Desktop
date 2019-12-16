60a8bb4f94486fca725bed18771beab5c4787f74
RyizEE01 <tommyboseren27607@gmail.com>
2019-12-17 03:28:06 +0900

    latihan3

---
+++ prtemuan3.py
@@ -0,0 +1,25 @@
+list1 = ['fisika', 'kimia', '1993', '2017']
+list2 = [1,2,3,4,5,6,7]
+
+print ("Tahun: ", list1[2])
+print (list2[4:7])
+
+nama =['alya', 'yana', 'luna', 'sayang']
+umur =[17,20,18,19]
+
+print(sum(umur))
+print("umur= ", nama[0],umur[0])
+print("umur= ", nama[2],umur[1])
+
+a="1","2","3"
+print(a)
+
+aku = [1,2,3,4,5]
+kamu =[10,12,14,16,18]
+
+print(sum(aku))
+print(sum(kamu))
+print("kita= ", sum(aku)+sum(kamu))
+
+kamus =  {'nama': 'tommy', 'umur': '17'}
+print("kamus['nama']: ", kamus['nama'])
